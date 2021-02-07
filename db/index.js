4e7f67f24f6869dd62855db8fa3f5fe5d6b6c468
Josh Rehanek <joshrehanek@gmail.com>
2021-02-06 21:13:37 -0800

    adds DB class holding view funcs & add dept & role

---
+++ db/index.js
@@ -0,0 +1,93 @@
+const connection = require('./connection');
+
+class DB {
+    constructor(connection) {
+        this.connection = connection;
+    }
+    viewAllDepartments() {
+        return this.connection.query(
+            ` 
+            SELECT 
+            department.id,
+            department.name 
+            
+            FROM 
+            department
+
+            ORDER BY
+            department.id
+            `
+        );
+    }
+    viewAllRoles() {
+        return this.connection.query(
+            `
+            SELECT 
+            role.id,
+            role.title,
+            role.salary,
+            department.name AS department
+            
+            FROM 
+            role
+
+            LEFT JOIN
+            department ON 
+            role.department_id = department.id 
+
+            ORDER BY
+            role.id
+
+            `
+        )
+    }
+    viewAllEmployees() {
+        return this.connection.query(
+            `
+            SELECT 
+            employee.id,
+            employee.first_name,
+            employee.last_name,
+            employee.role_id,
+            employee.manager_id
+            
+            FROM 
+            employee
+
+            LEFT JOIN
+            role ON 
+            employee.role_id = role.id 
+
+            LEFT JOIN
+            role ON
+            employee.manager_id = employee.id
+
+            ORDER BY
+            role.id
+
+            `
+        )
+    }
+    addDepartment(department) {
+        return this.connection.query(
+            `
+        INSERT INTO
+            department
+        SET
+            ?
+        `, department
+        );
+    }
+    addRole(role) {
+        return this.connection.query(
+            `
+            INSERT INTO
+                role  
+            SET
+                ?
+            `, role
+        );
+    }
+}
+
+module.exports = new DB(connection);

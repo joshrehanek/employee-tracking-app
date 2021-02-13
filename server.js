aabea432b0d4c6358b5b2f375f8c8844e694da72
Josh Rehanek <joshrehanek@gmail.com>
2021-02-12 18:14:29 -0800

    adds comments & deletes console logs

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// import module & dependencies
 require('console.table');
 const inquirer = require("inquirer");
 const db = require("./db");
 
+//mainMenu inquirer prompt
 const mainMenu = () => {
   inquirer
     .prompt({
@@ -16,16 +18,17 @@ const mainMenu = () => {
         'Add a new role.',
         'Add a new employee.',
         'Update employee roles.',
-        'Update employee managers.',
-        'View employees by manager.',
+        // 'Update employee managers.',
+        // 'View employees by manager.',
         'Delete department.',
         'Delete role.',
-        'Delete employees',
-        'View budget for department',
+        // 'Delete employees',
+        // 'View budget for department',
         'Exit',
         ''
       ],
     })
+    // case handling 
     .then((answer) => {
       switch (answer.action) {
         case 'View all departments.':
@@ -56,13 +59,13 @@ const mainMenu = () => {
           updateEmployeeRole();
           break;
 
-        case 'Update employee managers.':
-          updateEmployeeManager();
-          break;
+        // case 'Update employee managers.':
+        //   updateEmployeeManager();
+        //   break;
 
-        case 'View employees by manager.':
-          viewEmployeesByManager();
-          break;
+        // case 'View employees by manager.':
+        //   viewEmployeesByManager();
+        //   break;
 
         case 'Delete department.':
           deleteDepartment();
@@ -72,13 +75,13 @@ const mainMenu = () => {
           deleteRole();
           break;
 
-        case 'Delete employee.':
-          deleteEmployee();
-          break;
+        // case 'Delete employee.':
+        //   deleteEmployee();
+        //   break;
 
-        case 'View budget for department.':
-          viewBudget();
-          break;
+        // case 'View budget for department.':
+        //   viewBudget();
+        //   break;
 
         case 'Exit':
           db.connection.end();
@@ -93,28 +96,35 @@ const mainMenu = () => {
 
 // async function to view all departments
 async function viewAllDepartments() {
-  //departments variable which set to view all departments function from index.js in db folder
+  //departments variable which set to view all departments query function from index.js in db folder
   const departments = await db.viewAllDepartments();
   // console tables departments variable
   console.table(departments);
   // runs main menu prompt 
   mainMenu();
 }
-
+// async function to view all roles
 async function viewAllRoles() {
+  // roles variable set to viewAllRoles query function from index.js in db folder
   const roles = await db.viewAllRoles();
+  // console tables roles
   console.table(roles);
+  // runs main menu prompt
   mainMenu();
 }
-
+// async function to viewAllEmployees 
 async function viewAllEmployees() {
+  //employees variable set to view all query function from index.js in db folder
   const employees = await db.viewAllEmployees();
+  // console tables employees
   console.table(employees);
+    // runs main menu prompt
   mainMenu();
 }
 
+// async function to add department
 async function addDepartment() {
-
+// department = prompt answers
   const department = await inquirer.prompt([
     {
       type: 'input',
@@ -128,17 +138,22 @@ async function addDepartment() {
     },
     
   ])
+  //passes department var into/runs addDepartment query function from index.js
  await db.addDepartment(department);
+ //runs view all departments function
   viewAllDepartments();
 }
 
+//async function to add roles
 async function addRole() {
+  //departments = results from viewAllDepartments() in index.js
   const departments = await db.viewAllDepartments();
+  // maps over departments variable and pulls id & name to use in inquirer prompt
   const departmentChoices = departments.map(({ id, name }) => ({
     name: name,
     value: id
   }))
-
+  // role variable = inquirer prompt answers
   const role = await inquirer.prompt([
     {
       type: 'input',
@@ -157,18 +172,24 @@ async function addRole() {
       choices: departmentChoices
     }
   ])
+  // passes role variable into/runs addRole query
   await db.addRole(role);
+  //call viewAllRoles 
   viewAllRoles();
 }
 
+//async function to add employee
 async function addEmployee() {
+  //roles = results from viewAllEmployees() query
   const roles = await db.viewAllEmployees();
+  // maps over roles variable and pulls role_id to use in inquirer prompt
   const roleChoices = roles.map(({ role_id }) => ({
     name: role_id,
     value: role_id
   }))
-
+ //employees = results from viewAllEmployees() query
   const employees = await db.viewAllEmployees();
+   // maps over employees variable and pulls manager_id & title to use in inquirer prompt
   const managerIdChoices = employees.map(({ manager_id, title }) => ({
     name: title,
     value: manager_id
@@ -198,12 +219,17 @@ async function addEmployee() {
       choices: managerIdChoices
     }
   ])
+  // passes employee variable into/runs addEmployee query
   await db.addEmployee(employee);
+  // calls viewAllEmployees();
   viewAllEmployees();
 }
 
+//async function to update employee roles
 async function updateEmployeeRole() {
+ //roles = results from viewAllEmployees() query
   const roles = await db.viewAllEmployees();
+  // maps over roles variable and pulls role_id & title to use in inquirer prompt
   const roleChoices = roles.map(({ title, role_id }) => ({
     name: title,
     value: role_id
@@ -231,12 +257,13 @@ async function updateEmployeeRole() {
     }
     
   ])
-  console.log(employeeRole);
+  // passes employeRole variable into/runs updateEmployeeRole query
   await db.updateEmployeeRole(employeeRole);
+  //calls viewAllEmployees()
   viewAllEmployees();
 
 }
-
+//deltes departments
 async function deleteDepartment() {
 
   const deleteDepartment = await inquirer.prompt([
@@ -247,22 +274,17 @@ async function deleteDepartment() {
     },
  
   ])
-  console.log(deleteDepartment);
  await db.deleteDepartment(deleteDepartment);
   viewAllDepartments();
 }
 
+//deletes roles
 async function deleteRole() {
-  const departments = await db.viewAllDepartments();
-  const departmentChoices = departments.map(({ id, name }) => ({
-    name: name,
-    value: id
-  }))
 
   const deleteRole = await inquirer.prompt([
     {
       type: 'input',
-      name: 'title',
+      name: 'role',
       message: 'What role would you like to delete?'
     }
   ])
